c3fb37c26e93ebb77fd5457daff932f9d8c4586d
rfranca86 <universius@gmail.com>
2017-11-06 15:49:15 -0200

    versionando api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,11 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # configura o middleware inserindo antes o rack::cors, permitindo
+  # que qualquer origem chame a API.
+  allow do
+    origins '*' # qualquer origem chama a API
+    resource '*', # qualquer resource pode ser chamado dessa API
+      headers: :any, # qualquer header pode ser chamado
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'], # é necessario expor nos headers
+      methods: [:get, :post, :put, :patch, :delete, :options, :head] # methodos liberados
+  end
+end
