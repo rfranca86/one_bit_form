0e534ee3aac73c1a832bc2b2932ca45160af10d0
rfranca86 <universius@gmail.com>
2017-11-09 16:54:25 -0200

    created resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       mount_devise_token_auth_for 'User', at: 'auth'
+      resources :forms
+      resources :questions, only: [:create, :update, :destroy]
+      resources :answers, only: [:index, :show, :create, :destroy]
     end
   end
 end
-=begin
- utilizando o namespace ele acrescenta a url api e v1
- ficando dessa forma:
- localhost:3000/api/v1
-=end
-
