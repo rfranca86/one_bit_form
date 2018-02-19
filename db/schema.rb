6df4cea51403b07818610ff4a8ea7061d3725eed
rfranca86 <universius@gmail.com>
2018-02-19 02:52:36 -0300

    Create required field in question model and generate migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115144556) do
+ActiveRecord::Schema.define(version: 20180219010259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171115144556) do
     t.integer  "form_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "required"
     t.index ["form_id"], name: "index_questions_on_form_id", using: :btree
   end
 
